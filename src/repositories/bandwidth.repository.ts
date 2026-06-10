f47b9c244b192dc89b015645e098e7ba4d7829a8
Wardi <wardi@nusa.net.id>
2026-06-10 12:22:43 +0700

    fix: resolve SQL syntax error in bandwidth search for multiple IPs

--- src/repositories/bandwidth.repository.ts
+++ src/repositories/bandwidth.repository.ts
@@ -25,46 +25,53 @@ export class BandwidthRepository {
           const strings: string[] = []
           const values: string[] = []
 
+          strings.push(`
+            SELECT
+              cst.Network AS ip,
+              (ss.NormalDownCeil * 1000) AS download_rate,
+              (ss.NormalUpCeil * 1000) AS upload_rate,
+              'bps' AS unit,
+              cs.ServiceId AS subscription_package
+            FROM
+              CustomerServiceTechnical AS cst
+            LEFT JOIN
+              CustomerServices AS cs ON cs.CustServId = cst.CustServId
+            LEFT JOIN
+              Services AS s ON cs.ServiceId = s.ServiceId
+            LEFT JOIN
+              ServiceShaping AS ss ON cs.ServiceId = ss.ServiceId
+            WHERE
+              NOT (s.ServiceGroup IN ('SLBP', 'MT', 'IP'))
+              AND (
+                (cst.Network = `)
+
           for (let i = 0; i < batch.length; i++) {
             const ip = batch[i]
-            if (i === 0) {
-              strings.push(`
-                SELECT
-                  cst.Network AS ip,
-                  (ss.NormalDownCeil * 1000) AS download_rate,
-                  (ss.NormalUpCeil * 1000) AS upload_rate,
-                  'bps' AS unit,
-                  cs.ServiceId AS subscription_package
-                FROM
-                  CustomerServiceTechnical AS cst
-                LEFT JOIN
-                  CustomerServices AS cs ON cs.CustServId = cst.CustServId
-                LEFT JOIN
-                  Services AS s ON cs.ServiceId = s.ServiceId
-                LEFT JOIN
-                  ServiceShaping AS ss ON cs.ServiceId = ss.ServiceId
-                WHERE
-                  NOT (s.ServiceGroup IN ('SLBP', 'MT', 'IP'))
-                  AND (
-                    (cst.Network = `)
-            } else {
-              strings.push(`) OR (cst.Network = `)
-            }
+
+            values.push(ip)
 
             strings.push(` OR cst.Network = CONCAT(`)
+            values.push(ip)
+
             strings.push(
               `, '/32') OR (INSTR(cst.Network, '/') > 0 AND INET_ATON(`,
             )
+            values.push(ip)
 
-            values.push(ip, ip, ip)
+            if (i === batch.length - 1) {
+              strings.push(
+                `) BETWEEN INET_ATON(SUBSTRING_INDEX(cst.Network, '/', 1)) AND (INET_ATON(SUBSTRING_INDEX(cst.Network, '/', 1)) + POWER(2, 32 - CAST(SUBSTRING_INDEX(cst.Network, '/', -1) AS UNSIGNED)) - 1))))`,
+              )
+            } else {
+              strings.push(
+                `) BETWEEN INET_ATON(SUBSTRING_INDEX(cst.Network, '/', 1)) AND (INET_ATON(SUBSTRING_INDEX(cst.Network, '/', 1)) + POWER(2, 32 - CAST(SUBSTRING_INDEX(cst.Network, '/', -1) AS UNSIGNED)) - 1))) OR (cst.Network = `,
+              )
+            }
           }
-
-          strings.push(
-            `) BETWEEN INET_ATON(SUBSTRING_INDEX(cst.Network, '/', 1)) AND (INET_ATON(SUBSTRING_INDEX(cst.Network, '/', 1)) + POWER(2, 32 - CAST(SUBSTRING_INDEX(cst.Network, '/', -1) AS UNSIGNED)) - 1)))`,
-          )
 
           const stringsArray = strings as unknown as TemplateStringsArray
           stringsArray.raw = stringsArray
+          console.log("SQL QUERY:", stringsArray.join('?'))
 
           return sql(stringsArray, ...values)
         }),
